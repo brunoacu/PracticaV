697b2432de5c22936ef69a53faa6266008191550
brunoacu <brunoacuna364@gmail.com>
2023-07-01 18:38:08 -0300

    Modificamos el componente items

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import{Item }from "../../models/item"
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent {
-
+  items:Item[]=[];
+  constructor(){}
+  ngOnInit():void{
+    this.items=[  
+      {
+        id:0,
+        title:"manzana",
+        price:10.5,
+        quiantity:4,
+        completed:false
+      },
+      {
+        id:1,
+        title:"pan",
+        price:3.5,
+        quiantity:8,
+        completed:true
+      },
+    ]
+  }
 }
