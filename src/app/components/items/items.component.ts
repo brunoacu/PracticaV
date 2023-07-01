fcc62112425ad2c3f1f10d55e51c3b387d912332
brunoacu <brunoacuna364@gmail.com>
2023-07-01 20:15:31 -0300

    Editamos los componentes item y items

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import{Item }from "../../models/item"
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
-export class ItemsComponent {
+export class ItemsComponent implements OnInit {
+
   items:Item[]=[];
   constructor(){}
   ngOnInit():void{
@@ -14,16 +15,27 @@ export class ItemsComponent {
         id:0,
         title:"manzana",
         price:10.5,
-        quiantity:4,
+        quantity:4,
         completed:false
       },
       {
         id:1,
         title:"pan",
         price:3.5,
-        quiantity:8,
+        quantity:8,
         completed:true
       },
+      {
+        id:2,
+        title:"remera",
+        price:500,
+        quantity:1,
+        completed:false
+      },
     ]
+  }
+  deleteItem(item: Item){
+    this.items = this.items.filter(i => i.id != item.id);
+
   }
 }
--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -2,6 +2,6 @@ export class Item{
     id:number=0;
     title:string="";
     price:number=0;
-    quiantity:number=0;
+    quantity:number=0;
     completed: boolean=false;
 }
